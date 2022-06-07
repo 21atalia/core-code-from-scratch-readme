40d43f31d94ac146ee977baf61c73def2531a0ba
Atalia Moreno <101205494+21atalia@users.noreply.github.com>
2022-06-07 15:09:51 -0600

    Add files via upload

---
+++ examples/TypeScriptGuide/Define_generics/implement_generics_classes_interfacs.ts
@@ -0,0 +1,73 @@
+//Exercise - Implement generics with interfaces and classes
+
+//Declare a generic interface
+interface Identity<T, U> {
+    value: T;
+    message: U;
+}
+
+let returnNumber: Identity<number, string> = {
+    value: 25,
+    message: 'Hello!'
+}
+let returnString: Identity<string, number> = {
+    value: 'Hello!',
+    message: 25
+}
+
+//Declare a generic interface as a function type
+
+interface ProcessIdentity<T, U> {
+    (value: T, message: U): T;
+}
+
+function processIdentity<T, U> (value: T, message: U) : T {
+    console.log(message);
+    return value
+}
+
+let processor: ProcessIdentity<number, string> = processIdentity;
+let returnNumber1 = processor(100, 'Hello!');   // OK
+let returnString1 = processor('Hello!', 100);   // Type check error
+
+//Declare a generic interface as a class type
+
+interface ProcessIdentity<T, U> {
+    value: T;
+    message: U;
+    process(): T;
+}
+
+class processIdentity<X, Y> implements ProcessIdentity<X, Y> {
+    value: X;
+    message: Y;
+    constructor(val: X, msg: Y) {
+        this.value = val;
+        this.message = msg;
+    }
+    process() : X {
+        console.log(this.message);
+        return this.value
+    }
+}
+
+let processor = new processIdentity<number, string>(100, 'Hello');
+processor.process();           // Displays 'Hello'
+processor.value = '100';       // Type check error
+
+//Define a generic class
+
+class processIdentity<T, U> {
+    private _value: T;
+    private _message: U;
+    constructor(value: T, message: U) {
+        this._value = value;
+        this._message = message;
+    }
+    getIdentity() : T {
+        console.log(this._message);
+        return this._value
+    }
+}
+let processor = new processIdentity<number, string>(100, 'Hello');
+processor.getIdentity();      // Displays 'Hello'
